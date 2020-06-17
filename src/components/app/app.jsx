8f4146184950111de26fbdf5076527ac085a0587
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-06-17 19:33:35 +0300

    Опишет предварительную логику отображения экранов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,41 +1,91 @@
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import {GameType} from "../../const.js";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
-const welcomeButtonHandler = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: -1,
+    };
 
-const App = (props) => {
-  const {errorsCount, questions} = props;
+    this._switchToNextScreen = this._switchToNextScreen.bind(this);
+  }
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <WelcomeScreen
-            errorsCount = {errorsCount}
-            onWelcomeButtonClick = {welcomeButtonHandler}
-          />
-        </Route>
-        <Route exact path="/artist">
-          <ArtistQuestionScreen
-            question={questions[1]}
-            onAnswer={() => {}}
-          />
-        </Route>
-        <Route exact path="/genre">
-          <GenreQuestionScreen
-            question={questions[0]}
-            onAnswer={() => {}}
-          />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+  render() {
+    const {questions} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/artist">
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path="/genre">
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+  _renderGameScreen() {
+    const {errorsCount, questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+          errorsCount={errorsCount}
+          onWelcomeButtonClick={this._switchToNextScreen}
+        />
+      );
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <ArtistQuestionScreen
+              question={question}
+              onAnswer={this._switchToNextScreen}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <GenreQuestionScreen
+              question={question}
+              onAnswer={this._switchToNextScreen}
+            />
+          );
+      }
+    }
+
+    return null;
+  }
+
+  _switchToNextScreen() {
+    this.setState((prevState) => ({
+      step: prevState.step + 1
+    })
+    );
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
