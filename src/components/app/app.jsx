62eb5be547ebd50537014671d5cc40e8318cacbd
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-06-17 11:38:43 +0300

    Устанавливает и настраивает роутер react-router-dom

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,9 @@
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import React from "react";
 import PropTypes from "prop-types";
+
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 const welcomeButtonHandler = () => {};
@@ -8,10 +12,22 @@ const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen
-      errorsCount = {errorsCount}
-      onWelcomeButtonClick = {welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount = {errorsCount}
+            onWelcomeButtonClick = {welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/artist">
+          <ArtistQuestionScreen/>
+        </Route>
+        <Route exact path="/genre">
+          <GenreQuestionScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
