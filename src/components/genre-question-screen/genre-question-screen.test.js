c1f41beaad852163d828a1a071222835ab792d2b
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-06-17 18:17:09 +0300

    Добавляет тесты для компонента по жанру

---
+++ src/components/genre-question-screen/genre-question-screen.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import GenreQuestionScreen from "./genre-question-screen.jsx";
+
+const testQuestion = {
+  type: `genre`,
+  genre: `rock`,
+  answers: [{
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `blues`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `jazz`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }],
+};
+
+describe(`GenreQuestionScreen Component rendering`, () => {
+  it(`GenreQuestionScreen Component should render correctly`, () => {
+    const tree = renderer
+      .create(
+          <GenreQuestionScreen
+            question={testQuestion}
+            onAnswer={() => {}}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
