2f7397cd13042b0e286be866cff08dbb99eeb58d
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-07-06 10:39:22 +0300

    Подключает Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/app/app.jsx";
+import {reducer} from "./reducer/reducer.js";
 import questions from "./mocks/questions.js";
 
 const Settings = {
   ERRORS_COUNT: 3
 };
 
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
-    <App
-      errorsCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
